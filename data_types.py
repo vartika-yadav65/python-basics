0b7dc1f99189d4c372aeb53f341d48e324ce8c81
vartika-yadav65 <vartika2507@gmail.com>
2026-08-21 21:32:44 +0530

    Add python data dype

---
+++ data_types.py
@@ -0,0 +1,18 @@
+# Learning some basic data types in Python
+
+name = "Vartika"       # String
+age = 20               # Integer
+height = 5.2           # Float
+is_student = True      # Boolean
+
+# Printing the values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Checking the type of each value
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
