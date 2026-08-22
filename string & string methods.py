d607500128a75acee73928a89eb082bec66b387e
vartika-yadav65 <vartika2507@gmail.com>
2026-08-22 20:41:41 +0530

    Add strings & string methods

---
+++ string & string methods.py
@@ -0,0 +1,8 @@
+text = "  Hello World from Python  "
+
+print(text.strip())
+print(text.lower())
+print(text.upper())
+print(text.replace("World", "GitHub"))
+print(text.strip().split(" "))
+print(len(text))
