6fea7420c0a6b5903e56c885fff5e1e039f21097
vartika-yadav65 <vartika2507@gmail.com>
2026-08-22 19:43:49 +0530

    Add input&output

---
+++ input_output.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+print("Hey " + name + ", nice to meet you!")
+print(f"So you're {age} years old huh")
+
+# just messing around with output formatting
+total = 5 + 3
+print("5 + 3 =", total)
