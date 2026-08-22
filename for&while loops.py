da30607ea353803e2777cb0bd10bb0ed503efdbe
vartika-yadav65 <vartika2507@gmail.com>
2026-08-22 19:52:48 +0530

    Add for & while loops

---
+++ for&while loops.py
@@ -0,0 +1,12 @@
+# for loop - printing table of 5
+for i in range(1, 11):
+    print(f"5 x {i} = {5*i}")
+
+print("---")
+
+# while loop - countdown
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+print("Liftoff!")
