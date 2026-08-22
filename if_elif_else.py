43e2070a05c592db9c2fb1c338a36ca407309649
vartika-yadav65 <vartika2507@gmail.com>
2026-08-22 19:49:22 +0530

    Add if/elif/else

---
+++ if_elif_else.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("You failed bro, study harder")
