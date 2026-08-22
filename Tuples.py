0ae383db80e6757992bcc63d900b218190c4bfca
vartika-yadav65 <vartika2507@gmail.com>
2026-08-22 20:36:27 +0530

    Add tuples

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+coordinates = (10, 20)
+print("X:", coordinates[0])
+print("Y:", coordinates[1])
+
+# tuples can't be changed, learned that the hard way
+colors = ("red", "green", "blue")
+for c in colors:
+    print(c.upper())
